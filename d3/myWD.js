b7d825145e80fb536476fa5557facfd292e053ae
liuyuhsuan <yuhsuan0902@gmail.com>
2016-12-20 01:07:03 +0800

    1220

---
+++ d3/myWD.js
@@ -0,0 +1,12 @@
+function myWD()
+{
+d3.csv("https://liuyuhsuan.github.io/mypage./wc.csv", 
+      function(data)
+      {
+        d3.wordcloud()
+          .size([500, 300])
+          .fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
+          .words(data)
+          .start();
+      });
+}

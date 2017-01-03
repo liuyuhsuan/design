9cc4d12e4cf2db57f67cb8d5adb527f2ebaa4b0f
liuyuhsuan <yuhsuan0902@gmail.com>
2017-01-03 15:04:31 +0800

    0103

--- d3/myWD.js
+++ d3/myWD.js
@@ -4,7 +4,7 @@ d3.csv("https://liuyuhsuan.github.io/design/d3/wc.csv",
       function(data)
       {
         d3.wordcloud()
-          .size([500, 300])
+          .size([1000, 600])
           .fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
           .words(data)
           .start();
